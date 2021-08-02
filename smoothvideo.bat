d28739293a9f6c35947cf6f502cfb9b370a531ad
bjaan <42038171+bjaan@users.noreply.github.com>
2021-08-02 13:33:52 +0200

    improved settings (less artifacts & higher quality encoding)

--- smoothvideo.bat
+++ smoothvideo.bat
@@ -7,7 +7,7 @@ echo v_workingdir="%~dp0" > parameters.avs
 echo v_source="%~f1" >> parameters.avs
 
 rem smooth video
-"%FFMPEG_ROOT%\bin\ffmpeg.exe" -i smoothvideo.avs -vcodec libx265 -crf 28 -preset fast "%~d1%~p1%~n1_output_no_audio.mkv"
+"%FFMPEG_ROOT%\bin\ffmpeg.exe" -i smoothvideo.avs -pix_fmt yuv420p -vcodec libx265 -crf 15 -preset fast "%~d1%~p1%~n1_output_no_audio.mkv"
 
 rem merge audio with smoothed video file
 "%FFMPEG_ROOT%\bin\ffmpeg.exe" -i "%~d1%~p1%~n1_output_no_audio.mkv" -i "%~f1" -c:v copy -map 0:v:0 -map 1:a:0 -c:a copy -shortest "%~p1%~n1_output.mkv"
