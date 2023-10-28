3702fb1d56d3df6cb6d58eff286c72daedfd9053
bjaan <42038171+bjaan@users.noreply.github.com>
2023-10-28 03:37:24 +0200

    Added stabilize scripts that darkens lines

---
+++ smoothvideo_stabilize.bat
@@ -0,0 +1,27 @@
+@echo off
+set FFMPEG_ROOT=D:\Apps\ffmpeg-2023-10-12-git-a7663c9604-full_build
+
+rem path tilde tricks see https://ss64.com/nt/syntax-args.html
+
+if "%~1" == "" goto usage
+
+echo v_workingdir="%~dp0" > %~dp0\parameters.avs
+echo v_source="%~f1" >> %~dp0\parameters.avs
+echo on
+
+rem smooth video
+"%FFMPEG_ROOT%\bin\ffmpeg.exe" -i %~dp0\smoothvideo_stabilize.avs -pix_fmt yuv420p -vcodec libx265 -crf 15 -preset fast "%~d1%~p1%~n1_smooth_stabilized_no_audio.mkv"
+
+rem merge audio with smoothed video file
+"%FFMPEG_ROOT%\bin\ffmpeg.exe" -i "%~d1%~p1%~n1_smooth_stabilized_no_audio.mkv" -i "%~f1" -c:v copy -map 0:v:0 -map 1:a:0 -c:a copy -shortest "%~d1%~p1%~n1_smooth_stabilized.mkv"
+
+del "%~f1.ffindex"
+
+goto end
+
+:usage
+echo Smooth a video file
+echo smoothvideo_stabilize.bat videofile_with_audio.mp4
+echo   videofile_with_audio.mp4: take the audio from this file and smooth the video of it in the resulting video file
+
+:end
